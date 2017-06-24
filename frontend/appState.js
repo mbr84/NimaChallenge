0692df79aee8185455944714b8237184621fdf49
mbr84 <matthew.bramfeld@gmail.com>
2017-06-24 16:38:28 -0700

    app state store

--- frontend/appState.js
+++ frontend/appState.js
@@ -1,39 +1,70 @@
 import Rx from 'rxjs';
 import { Map, List } from 'immutable';
+import { localStorageAsync } from './utils'
 import $ from 'jquery'
 
 
 const initializeAppState = () => {
-  const initialState = new Map({ adding: false, cars: new List() })
+  const nextPageSubject = new Rx.Subject()
+  const nextPageClickStream = nextPageSubject
+    .map(() => state => {
+      const newPage = +state.get('currentPage') + 1
+      localStorageAsync(newPage)
+      return state.set('currentPage', newPage)
+    })
+
+  const lastPageSubject = new Rx.Subject()
+  const lastPageClickStream = lastPageSubject
+    .map(() => state => {
+      const newPage = state.get('currentPage') - 1
+      localStorageAsync(newPage)
+      return state.set('currentPage', newPage)
+    })
+
+  const initialState = new Map({
+    nextPage: () => nextPageSubject.next(),
+    lastPage: () => lastPageSubject.next(),
+    totalPages: 0,
+    isAdding: false,
+    cars: new List(),
+    currentPage: window.localStorage.getItem('currentPage') || 0
+  })
+
   const addCarClickStream = Rx.Observable.fromEvent($('form'), 'submit')
   const formSubmissionStream = addCarClickStream.share()
     .forEach(e => e.preventDefault())
 
+  const inputBlurStream = Rx.Observable.fromEvent($('input'), 'blur')
+  inputBlurStream.merge(Rx.Observable.fromEvent($('button'), 'mousedown'))
+    .forEach(e => $(e.currentTarget).attr('required', true))
+
   const requestingStream = addCarClickStream.share()
-    .map(() => state => state.set('adding', true))
+    .map(() => state => state.set('isAdding', true))
 
   const requestStream = addCarClickStream.mapTo('http://localhost:3000/cars')
 
   const responseStream = requestStream.flatMap(requestUrl => {
-    const make = $('#make').val()
-    const model = $('#model').val()
-    const year = $('#year').val()
-    const data = { make, model, year }
+    const data = { make: $('#make').val(), model: $('#model').val(), year: $('#year').val() }
     return Rx.Observable.fromPromise($.ajax({url: requestUrl, method: 'post', data: data }))
   })
 
   const refreshCarListStream = responseStream
-    .startWith("")
+    .startWith(null)
     .flatMap(() => {
       return Rx.Observable.fromPromise($.ajax('http://localhost:3000/cars'))
     })
-    .map(res => state => state.set('cars', new List(res.data)).set('adding', false))
+    .map(res => state =>
+      state.set('cars', new List(res.data))
+        .set('isAdding', false)
+        .set('totalPages', Math.floor(res.data.length / 15)))
 
     return Rx.Observable.merge(
-        requestingStream,
-        refreshCarListStream
-      )
-      .scan((acc, curr) => curr(acc), initialState)
+      lastPageClickStream,
+      nextPageClickStream,
+      requestingStream,
+      refreshCarListStream
+    )
+    .scan((acc, curr) => curr(acc), initialState)
 }
 
 export default initializeAppState
