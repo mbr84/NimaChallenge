6a7a7119b35a4bd69d97f0bf4fc18aca4e286ad5
mbr84 <matthew.bramfeld@gmail.com>
2017-06-24 16:38:53 -0700

    add car component

--- frontend/components/addCarForm.js
+++ frontend/components/addCarForm.js
@@ -1,19 +1,34 @@
-import { form, input }
+import { form, input, button, div } from 'snabbdom-helpers'
+import h from 'snabbdom/h'
+import $ from 'jquery'
 
-const newCarForm = form({
-  inner: [
-    input({
-      attrs: { type: text, placeholder: "Make"}
-    }),
-    input({
-      attrs: { type: text, placeholder: "Model"}
-    }),
-    input({
-      attrs: { type: text, placeholder: "Year"}
-    }),
-    input({
-      attrs: { type: submit } ,
-      inner: ["ADD"]
-    })
-  ]
-})
+const addCarForm = isAdding =>{
+  const buttonText = isAdding ? "ADDING" : "ADD"
+
+  return form({
+    inner: [
+      input({
+        props: { type: 'text', id:'make', placeholder: "Make"},
+
+      }),
+      input({
+        props: { type: 'text', id:'model', placeholder: "Model"},
+
+      }),
+      input({
+        props: {type: 'number', id: 'year', min:"1930", max:"2018", placeholder: "Year"},
+
+      }),
+      button({
+        props: { type: 'submit' },
+        inner: [buttonText]
+      }),
+      div({ props: { className: 'error', id: 'make-error' }, inner: "Please Fill in this Field" }),
+      div({ props: { className: 'error', id: 'model-error' }, inner: "Please Fill in this Field" }),
+      div({ props: { className: 'error', id: 'year-error' }, inner: "Please add a Valid Year" }),
+      div({ props: { className: 'error' } }),
+
+    ]
+  })
+}
+export default addCarForm
