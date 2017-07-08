1a33ddd007888c3b8b3d67759734e4c33311a9ca
mbr84 <matthew.bramfeld@gmail.com>
2017-07-08 01:08:14 -0700

    testing another setup of fn

--- frontend/bundle.js
+++ frontend/bundle.js
@@ -78709,7 +78709,7 @@ var chartState = function chartState() {
 
     return {
         toggleChart: function toggleChart(e) {
-            console.log(e);toggleChartSubject.next(e.currentTarget.id);
+            console.log(e);toggleChartSubject.next(e.target.id);
         },
         chartStreams: _rxjs2.default.Observable.merge(toggleChartOff$, toggleChartOn$)
     };
